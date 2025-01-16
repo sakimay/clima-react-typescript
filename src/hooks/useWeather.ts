4b3fb6be02a960d779b480b39c2c10106801740b
Isaac <mayne.isaac@gmail.com>
2025-01-16 22:56:05 +0100

    test valibot

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,18 +1,31 @@
 import axios from "axios";
 import { SearchType } from "../types";
 import { z } from "zod";
+// import { object, string, number, InferOutput, parse } from "valibot";
 
 //zod
-const Weather = z.object({
-    name: z.string(),
-    main: z.object({
-        temp: z.number(),
-        temp_min: z.number(),
-        temp_max: z.number()
-    })
-})
+// const Weather = z.object({
+//     name: z.string(),
+//     main: z.object({
+//         temp: z.number(),
+//         temp_min: z.number(),
+//         temp_max: z.number()
+//     })
+// })
+//type Weather = z.infer<typeof Weather>
 
-type Weather = z.infer<typeof Weather>
+//valibot
+// const WeatherSchema = object({
+//     name: string(),
+//     main: object({
+//         temp: number(),
+//         temp_min: number(),
+//         temp_max: number()
+//     })
+// })
+
+// type Weather = InferOutput<typeof WeatherSchema>
+
 
 export default function useWeather() {
 
@@ -26,11 +39,20 @@ export default function useWeather() {
 
             const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
             const { data: weatherResult } = await axios.get(weatherUrl)
-            const result = Weather.safeParse(weatherResult)
-            if(result.success) {
-                console.log(result.data.name);
-                
-            }
+
+            //ZOD
+            // const result = Weather.safeParse(weatherResult)
+            // if(result.success) {
+            //     console.log(result.data.name);
+            // }
+
+            //VALIBOT
+            // const result = parse(WeatherSchema, weatherResult)
+            // if (result) {
+            //     console.log(result.name);
+            // }else{
+            //     throw new Error("No se pudo obtener el clima");  
+            // }
             
         } catch (error) {
             console.error(error);
